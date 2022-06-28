3d5870b5448a334b19c431a6d0ba12d7132febf3
vrmchk <ihorveremchuk8@gmail.com>
2022-06-28 23:00:06 +0300

    Added IFieldGenerator interface

---
+++ Nonogram/Nonogram/Models/IFieldGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Nonogram.Models;
+
+internal interface IFieldGenerator
+{
+    public List<Cell> Cells { get; }
+    public List<int> ColorsCounts { get; }
+}
